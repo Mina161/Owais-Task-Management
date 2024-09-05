67e50b3182a098cd5ab9d54d325a02ca7686c6eb
Mina Khalil <mina.ehab.khalil@gmail.com>
2024-09-05 18:38:30 +0300

    Updated storage to firebase-admin

--- backend/config/firebase-admin.js
+++ backend/config/firebase-admin.js
@@ -1,12 +1,15 @@
 var admin = require("firebase-admin");
 const { decryptFile } = require("../controllers/encryptionController");
+const { getStorage } = require("firebase-admin/storage");
 var serviceAccount = decryptFile()
 
 const app = admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
+  storageBucket: 'mk-todo-list-owais.appspot.com'
 });
 
 const firestore = admin.firestore();
-const storage = admin.storage();
+const storage = getStorage();
+const bucket = storage.bucket();
 
-module.exports = { firestore, storage }
+module.exports = { firestore, bucket, storage }
--- backend/controllers/storageController.js
+++ backend/controllers/storageController.js
@@ -1,15 +1,15 @@
-const { ref, getDownloadURL, deleteObject } = require("firebase/storage");
-const { storage } = require("../config/firebase-admin");
+const { getDownloadURL } = require("firebase-admin/storage");
+const { storage, bucket } = require("../config/firebase-admin");
 
 async function getMediaURL(media) {
-    const mediaRef = ref(storage, media);
+    const mediaRef = bucket.file(media);
     const mediaURL = await getDownloadURL(mediaRef);
     return mediaURL;
 }
 
 async function deleteMediaObject(media) {
-    const mediaRef = ref(storage, media);
-    return await deleteObject(mediaRef);
+    const mediaRef = bucket.file(media);
+    return await mediaRef.delete();
 }
 
 module.exports = { getMediaURL, deleteMediaObject }
